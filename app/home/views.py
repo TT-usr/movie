f037cd4a975c1d41a57faf7ac66b219b809d466e
JackYao <yaotiancheng@geekpark.net>
2017-12-12 15:41:27 +0800

    add play page

--- app/home/views.py
+++ app/home/views.py
@@ -3,7 +3,6 @@
 from . import home
 from flask import render_template, redirect, url_for
 
-
 @home.route("/")
 def index():
     return render_template("home/index.html")
@@ -52,3 +51,12 @@ def loginlog():
 @home.route("/moviecol/")
 def moviecol():
     return render_template("home/moviecol.html")
+
+
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
