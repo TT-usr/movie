df74b4105b23ec5644192ea12324d26831116448
JackYao <yaotiancheng@geekpark.net>
2017-12-12 16:43:54 +0800

    add page_not_found

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from flask import Flask 
+from flask import Flask, render_template
+
 app = Flask(__name__)
 app.debug = True
 
@@ -9,3 +10,8 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
